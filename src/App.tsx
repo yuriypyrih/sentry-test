7cd4300383e72b2a561ab6cc126e99c9a06c784c
Yuriy Pyrih <yuriypyrih@gmail.com>
2022-12-11 22:23:57 +0100

    sentry testttt

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>SENTRY TEST</div>
+        <div>SENTRY TEST ok</div>
         <button onClick={onClick}>SENTRY BUG</button>
       </header>
     </div>
